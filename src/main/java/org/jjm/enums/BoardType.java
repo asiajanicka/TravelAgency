382f9a14e81c80de3ca4d1ad6dacbf753fa14711
asiajanicka <asia.janicka@gmil.com>
2022-12-20 12:47:00 +0100

    moved prices from Board class to BoardType enum; deleted Board class

--- src/main/java/org/jjm/enums/BoardType.java
+++ src/main/java/org/jjm/enums/BoardType.java
@@ -1,19 +1,31 @@
 package org.jjm.enums;
 
+import java.math.BigDecimal;
 
 public enum BoardType {
-    BB("Bed&Breakfast"),
-    HB("Half Board"),
-    FB("Full Board"),
-    ALL_INCLUSIVE("All inclusive");
+    BB("Bed&Breakfast", "100"),
+    HB("Half Board", "125"),
+    FB("Full Board", "250"),
+    ALL_INCLUSIVE("All inclusive", "500");
 
     private final String displayName;
+    private final BigDecimal price;
 
-    BoardType(String s) {
-        displayName = s;
+    BoardType(String displayName, String price) {
+        this.displayName = displayName;
+        this.price = new BigDecimal(price);
     }
 
     public String getDisplayName() {
         return displayName;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Board type: %s", displayName);
     }
 }
